04888ceae17a7c635611f0b122a2bc9a82796045
RahulR100 <rahulrao@email.com>
2021-11-19 23:05:29 -0500

    socket on 4000

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io("/");
+const socket = io("/:4000");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 const showChat = document.querySelector("#showChat");
