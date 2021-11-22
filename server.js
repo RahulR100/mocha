892122546d4f30a2e5b2ff1b3ef1d38c8732c597
RahulR100 <rahulrao@email.com>
2021-11-21 22:55:33 -0500

    test on lan

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.get("/:room", (req, res) => {
 io.on("connection", (socket) => {
 	socket.on("join-room", (roomId, userId, userName) => {
 		socket.join(roomId);
-		socket.broadcast.emit("user-connected", userId);
+		io.to(roomId).emit("user-connected", userId);
 		socket.on('disconnect', () => {
-	      	socket.broadcast.emit('user-disconnected', userId);
+	      	io.to(roomId).emit('user-disconnected', userId);
 	    });
 		socket.on("message", (message) => {
 			io.to(roomId).emit("createMessage", message, userName);
